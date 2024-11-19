f8b3063273d9c49057648a5841d49372ac5d1b87
Pablo Sautner <pablo.sautner@mouts.info>
2024-11-18 23:07:10 -0300

    Adiciona método email

--- front-end/src/app/api/user/route.ts
+++ front-end/src/app/api/user/route.ts
@@ -1,5 +1,8 @@
 import { prisma } from '@/lib/prisma'
 import { NextResponse } from 'next/server'
+import sendgrid from '@sendgrid/mail'
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY!) 
 
 export async function POST(request: Request) {
   try {
@@ -28,6 +31,30 @@ export async function POST(request: Request) {
         celular: numero
       },
     })
+
+    const html_content = `
+    <html>
+        <body style="font-family: Arial, sans-serif; color: #333;">
+            <div style="background-color: #f4f4f4; padding: 20px;">
+                <h2 style="color: #4CAF50;">Bem-vindo(a), ${nome}!</h2>
+                <p>Estamos muito felizes por ter você conosco.</p>
+                <p>Se precisar de alguma coisa, estamos à disposição!</p>
+                <p style="margin-top: 20px;">Atenciosamente,</p>
+                <p style="font-weight: bold;">Equipe Kresaut</p>
+            </div>
+        </body>
+    </html>
+    `
+
+    // Enviando o e-mail de boas-vindas 
+    const msg = {
+      to: email,
+      from: process.env.SENDER_EMAIL!, 
+      subject: 'Bem-vindo(a) à Kresaut!',
+      html: html_content, 
+    }
+
+    await sendgrid.send(msg)
 
     return NextResponse.json(newUser, { status: 201 })
   } catch (error) {
